858eb839ee4d168c591489950da67608f05c8cd7
Willy Huang <breezekiller789@csie.io>
2019-12-24 10:02:33 +0800

    12/25_HW4

---
+++ hw4/curl.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <math.h>
+#include <fcntl.h>
+#define MaxLine 1024
+
+int main(int argc, char *argv[]){
+    system("curl https://esldlep.cs.ccu.edu.tw:9000/");
+    
+
+    return 0;
+}
---
+++ hw4/print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <math.h>
+#include <fcntl.h>
+#define MaxLine 1024
+
+int main(int argc, char *argv[]){
+
+    printf("42\n0 0 7 32\n1 4 9 18\n2 1 2 8\n3 7 5 39\n4 2 5 12\n5 6 8 13\n6 10 4 18\n");
+
+    return 0;
+}
